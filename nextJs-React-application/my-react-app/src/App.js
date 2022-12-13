e030e7c91af580543353e858a3ea78c529cf20c5
Deepmala Kumari <deepmfnu@deloitte.com>
2022-12-13 11:26:07 +0530

    adding initial changes

---
+++ nextJs-React-application/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+  <div>
+    <h1>My Todoss</h1>
+     <Todo text='learn React'/>
+     <Todo text='master react'/>
+     <Todo text='explore the full react course'/>
+    </div>
+  )
+}
+
+export default App;
